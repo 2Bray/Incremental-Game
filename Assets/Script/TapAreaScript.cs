dea24811b118d8d43d6a92849c3f2483e3e94bf8
2Bray <17190440@bsi.ac.id>
2021-09-26 08:48:11 +0700

    Initial commit

---
+++ Assets/Script/TapAreaScript.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TapAreaScript : MonoBehaviour, IPointerDownHandler
+{
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        GameManagerScript.Instance.CollectByTap(eventData.position, transform);
+    }
+}
